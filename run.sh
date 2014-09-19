8580234102c287b94a5493db4f881593c5146f90
Benjamin Dreux <benjamin.dreux@code3.ca>
2014-09-19 14:30:25 -0400

    complete exemple of seach bounced

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
-jade src/*.jade --out webapp
-http-server webapp
+rm -rf webapp
+jade src/*.jade --out webapp --pretty
+mkdir webapp/js
+cp bower_components/highland/dist/highland.js webapp/js/
+cp bower_components/jquery/dist/jquery.js webapp/js/
+http-server --cors webapp
